f220d196f28cc87c0adc5d89ec5443a6e52dfafc
Anh Thu <thuthuanh19@gmail.com>
2023-02-17 19:51:25 +0700

    code UI

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState } from 'react';
 
 function App() {
+    const [tasks, setTasks] = useState(["abc"]);
   return (
     <div className="App">
-      
+      <h1 className='text-center'>TODO APP</h1>
+      <form className='text-center'>
+        <input type="text"/>
+        <button className='m-2 btn btn-info'>Add</button>
+      </form>
+
+      <ul className='mx-5'>
+        {tasks.map((item, index) => {
+            return <li key={index}>{item}</li>
+        })}
+      </ul>
     </div>
   );
 }
