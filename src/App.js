7ef933378754ee037ea33eb4683c3e945749f0c0
Anh Thu <thuthuanh19@gmail.com>
2023-02-17 21:27:13 +0700

    Add task function

--- src/App.js
+++ src/App.js
@@ -2,18 +2,35 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { useState } from 'react';
 
 function App() {
-    const [tasks, setTasks] = useState(["abc"]);
+    const [tasks, setTasks] = useState([]);
+    const [value1, setValue1] = useState("");
+
+    const handleAddTask = (e, input) => {
+        e.preventDefault();
+        const newTask = {
+            text: input,
+            completed: false
+        }
+        tasks.push(newTask);
+        setTasks(tasks);
+        setValue1("");
+    }
   return (
     <div className="App">
       <h1 className='text-center'>TODO APP</h1>
-      <form className='text-center'>
-        <input type="text"/>
+      <form className='text-center' onSubmit={(e) => handleAddTask(e, value1)}>
+        <input type="text" value={value1} onChange={(e) => setValue1(e.target.value)}/>
         <button className='m-2 btn btn-info'>Add</button>
       </form>
 
       <ul className='mx-5'>
         {tasks.map((item, index) => {
-            return <li key={index}>{item}</li>
+            return <li key={index}>
+                {item.text}
+                    <button className='btn btn-success m-2'>Completed</button>
+                    <button className='btn btn-primary me-2'>Edit</button>
+                    <button className='btn btn-danger'>Delete</button>
+                </li>
         })}
       </ul>
     </div>
